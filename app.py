a41160615728063593a466c45be07d77dc87b2fe
silkk-nn <70426262+silkk-nn@users.noreply.github.com>
2022-02-24 16:12:13 +0700

    app.py auto gpu and cpu

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ import pandas as pd
 import torch.nn as N
 import torch.optim as O
 
-#cell 8
+device = T.device("cuda" if T.cuda.is_available() else "cpu")
+
 idx2char = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '=', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', ']', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\xa0', '®', 'é', 'ü', 'ก', 'ข', 'ฃ', 'ค', 'ฅ', 'ฆ', 'ง', 'จ', 'ฉ', 'ช', 'ซ', 'ฌ', 'ญ', 'ฎ', 'ฏ', 'ฐ', 'ฑ', 'ฒ', 'ณ', 'ด', 'ต', 'ถ', 'ท', 'ธ', 'น', 'บ', 'ป', 'ผ', 'ฝ', 'พ', 'ฟ', 'ภ', 'ม', 'ย', 'ร', 'ฤ', 'ล', 'ฦ', 'ว', 'ศ', 'ษ', 'ส', 'ห', 'ฬ', 'อ', 'ฮ', 'ฯ', 'ะ', 'ั', 'า', 'ำ', 'ิ', 'ี', 'ึ', 'ื', 'ุ', 'ู', 'ฺ', '฿', 'เ', 'แ', 'โ', 'ใ', 'ไ', 'ๅ', 'ๆ', '็', '่', '้', '๊', '๋', '์', 'ํ', '๎', '๏', '๐', '๑', '๒', '๓', '๔', '๕', '๖', '๗', '๘', '๙', '๚', '๛', '\u200e', '–', '—', '‘', '’', '…', '™']
 
 char2idx={v:k for k,v in enumerate(idx2char)}
 
-device = T.device("cuda" if T.cuda.is_available() else "cpu")
-device
+
 
 def str2idxseq(charseq):
     idxseq = []
@@ -95,12 +95,10 @@ class WordsegModel(N.Module):
             brkvecs = self._log_softmax(statevecs)
             # print('brkvecs =\n{}'.format(brkvecs))
             return brkvecs
-        # except RuntimeError:
-        #     raise RuntimeError(statevecs)
 
 wordseg_model = WordsegModel(dim_charvec=16, dim_trans=32, no_layers=3)
 #ใส่ path model
-wordseg_model.load_state_dict(T.load('Onproduct_bigru.pt',map_location='cpu'))
+wordseg_model.load_state_dict(T.load('Onproduct_bigru.pt',map_location=device))
 wordseg_model.eval()
 
 def tokenize(wordseg_model, charseq):
